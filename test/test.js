2dd40a12cf059d031059c7e2a95ddb73406216f5
appalse <natalia.kutina13@gmail.com>
2019-12-25 16:17:35 +0300

    move tests in test folder

---
+++ test/test.js
@@ -0,0 +1,37 @@
+var lint = require('./../build/linter.js');
+console.log(typeof lint);
+console.log(lint);
+var chai = require('chai');
+var assert = chai.assert;
+var expect = chai.expect;
+var should = chai.should();
+
+describe('First test', () => {
+    it('some text', () => {
+        const json = `{
+            "block": "warning",
+            "content": [
+                {
+                    "block": "placeholder",
+                    "mods": { "size": "m" }
+                },
+                {
+                    "elem": "content",
+                    "content": [
+                        {
+                            "block": "text",
+                            "mods": { "size": "m" }
+                        },
+                        {
+                            "block": "text",
+                            "mods": { "size": "l" }
+                        }
+                    ]
+                }
+            ]
+        }`;
+        const result = lint.lint(json);
+        expect(result).to.be.an('array');
+        expect(result).to.equal([]);
+    }) 
+})
