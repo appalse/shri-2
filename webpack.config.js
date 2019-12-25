2e70fa2cef602a0a833c1eddc34899408685adc6
appalse <natalia.kutina13@gmail.com>
2019-12-25 16:18:33 +0300

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    plugins: [
+              new CleanWebpackPlugin()
+            ],
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'linter.js',
+        library: 'lint',
+        libraryTarget: 'commonjs',
+    }
+  };
