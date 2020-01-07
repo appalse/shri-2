92bc79d1eeba794de8f1848df2e1a1ded98f57f4
appalse <natalia.kutina13@gmail.com>
2020-01-07 14:21:35 +0300

    make json input valid

--- test/test-text-several-h1.js
+++ test/test-text-several-h1.js
@@ -8,18 +8,21 @@ describe('TEXT.SEVERAL_H1', () => {
     describe('INVALID', () => {
        
         it('2 sequential h1', () => {
-                const inputJson = `{[
+            const inputJson = `{
+                "block": "page",
+                "content": [
                     { "block": "text", "mods": { "type": "h1" } },
                     { "block": "text", "mods": { "type": "h1" } }
-                ]}`;
+                ]
+            }`;
             const result = lint(inputJson);
             expect(result).to.be.an('array').that.is.deep.equal([
                 {
                     "code": "TEXT.SEVERAL_H1",
                     "error": "Заголовок первого уровня должен быть единственным на странице",
                     "location": {
-                        "start": { "column": 21, "line": 3 },
-                        "end": { "column": 66, "line": 3 }
+                        "start": { "column": 21, "line": 5 },
+                        "end": { "column": 66, "line": 5 }
                     }
                 }
             ]);
